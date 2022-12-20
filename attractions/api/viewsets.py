0cd4cf011c0258f3b43bbb7c58515f173c827fe5
Italo Nobre <italonobre@ads.fiponline.edu.br>
2022-12-20 00:22:00 -0300

    implements Django Filter in the Attractions

--- attractions/api/viewsets.py
+++ attractions/api/viewsets.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
 from attractions.models import Attraction
 from .serializers import AttractionSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 class AttractionViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = Attraction.objects.all()
     serializer_class = AttractionSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name', 'description']

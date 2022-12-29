b8c5b40a3337fc030f04545ea9c38fbcdc01750d
Italo Nobre <italonobre@ads.fiponline.edu.br>
2022-12-29 09:00:29 -0300

    git commit Tokens

--- attractions/api/viewsets.py
+++ attractions/api/viewsets.py
@@ -1,12 +1,13 @@
+from warnings import filters
 from rest_framework import viewsets
 from attractions.models import Attraction
 from .serializers import AttractionSerializer
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 class AttractionViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = Attraction.objects.all()
     serializer_class = AttractionSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['name', 'description']
+    filter_backends = [filters.SearchFilter]
+    search_fields = ('name','=description')

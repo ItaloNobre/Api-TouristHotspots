70d089f234922b31fb98a83303ec7055e14b2756
Italo Nobre <italonobre@ads.fiponline.edu.br>
2022-12-29 23:16:27 -0300

    NestedSerializers

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework.fields import SerializerMethodField
 from core.models import TouristHotspot
+from attractions.api.serializers import AttractionSerializer
+from comments.api.serializers import CommentSerializer
+from assessments.api.serializers import AssessmentSerializer
+from addresses.api.serializers import AddressSerializer
+from rest_framework import serializers
 
 
 class TouristHotspotSerializer(ModelSerializer):
+
+    attractions = AttractionSerializer(many=True)
+    comments = CommentSerializer(many=True)
+    assessments = AssessmentSerializer(many=True)
+    addresses = AddressSerializer()
+    full_description = serializers.SerializerMethodField()
+    
     class Meta:
         model = TouristHotspot
-        fields = ['id','name', 'description','photo']
+        fields = ['id','name', 'description','approved','attractions','comments','assessments','addresses','full_description','full_description2']
+
+    def get_full_description(self, obj):
+        return '%s - %s' %(obj.name, obj.description )
--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -12,55 +12,13 @@ from rest_framework.authentication import TokenAuthentication
 class TouristHotspotViewSet(viewsets.ModelViewSet):
 
     serializer_class = TouristHotspotSerializer
+    queryset = TouristHotspot.objects.all()
     filter_backends = [filters.SearchFilter]
     permission_classes = [DjangoModelPermissions,]
     authentication_classes = [TokenAuthentication]
     search_fields = ('name','description')
     
-    def get_queryset(self):
-          id = self.request.query_params.get('id', None)
-          nome = self.request.query_params.get('name', None)
-          descricao = self.request.query_params.get('description', None)
-          queryset = TouristHotspot.objects.all()
-
-          if id:
-               queryset = TouristHotspot.objects.filter(pk=id)
-
-          if nome:
-               queryset = queryset.filter(nome__iexact=nome)
-
-          if descricao:
-               queryset = queryset.filter(descricao__iexact=descricao)
-
-          return queryset
-    def get_queryset(self):
-        return TouristHotspot.objects.filter(approved=True)
-
-    def list(self, request, *args, **kwargs):
-         return super().list(request, *args, **kwargs)
-    
-    def create(self, request, *args, **kwargs):
-         return super().create(request, *args, **kwargs)
-    
-    def destroy(self, request, *args, **kwargs):
-         return super().destroy(request, *args, **kwargs)
-
-    def retrieve(self, request, *args, **kwargs):
-         return super().retrieve(request, *args, **kwargs)
-    
-    def update(self, request, *args, **kwargs):
-         return super().update(request, *args, **kwargs)
-
-    def partial_update(self, request, *args, **kwargs):
-         return super().partial_update(request, *args, **kwargs)
-    
-    @action(methods=['get'], detail=True)
-    def denunciar(self, request, pk=None):
-          pass
-    
-    @action(methods=['post'], detail=False)
-    def teste(self,request):
-          pass
+   
 
 
    
